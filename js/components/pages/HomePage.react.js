0af35a09cc3ba2518a97b877e55493e7d5ebbcd1
bkthiruv <bkthiruv@jcp.com>
2016-11-22 17:18:38 +0530

    initial commit

---
+++ js/components/pages/HomePage.react.js
@@ -0,0 +1,111 @@
+/*
+ * HomePage
+ *
+ * This is the first thing users see of the app
+ * Route: /
+ *
+ */
+
+import React, { Component } from 'react';
+import { Link } from 'react-router';
+import Nav from '../Nav.react';
+import { connect } from 'react-redux';
+
+class HomePage extends Component {
+	render() {
+    const dispatch = this.props.dispatch;
+    const { loggedIn } = this.props.data;
+
+    return (
+			<article>
+				<div>
+					<section className="text-section">
+						{/* Change the copy based on the authentication status */}
+						{loggedIn ? (
+							<h1>You are logged in!</h1>
+						) : (
+							<h1>Welcome to Login Flow!</h1>
+						)}
+						<p>This initiative is basically to demonstrate the end to end application architecture including microservices ,API gateway and a powerfull UX layer</p>
+						<p>To get the code behind this application, go to <a href="https://github.com/bibinkt">Github</a></p>
+						<p>If your Gateway & microservice server has not configured then plz point this application login flow to Fakerequest.js . </p>
+						<p>To point to the fake response server , uncomment line number 62 & 130 from actions/AppActions.js</p>
+						{loggedIn ? (
+							<Link to="/dashboard" className="btn btn--dash">Dashboard</Link>
+						) : (
+							<div>
+								<Link to="/login" className="btn btn--login">Login</Link>
+								<Link to="/register" className="btn btn--register">Register</Link>
+							</div>
+						)}
+					</section>
+					<section>
+						<p>To have a look at the code behind this application, go to <a href="https://github.com/bibinkt">Github</a></p>
+						<p>To configure & run it locally: follow the below steps</p>
+						<h2> Install Node V6.9.1 </h2>
+						<ol>
+							<li><p> go to https://nodejs.org/en/download </p></li>
+							<li><p>Select LTS [current release] to install the dependencies.</p></li>
+							<li><p>Select your OS and click on installer</p></li>
+						</ol>
+						<h2> Install JAVA 1.8 </h2>
+						<h2> Install Cassandra[NOSQL] V3.9.0 </h2>
+						<ol>
+							<li><p>go to https://academy.datastax.com/planet-cassandra/cassandra  </p></li>
+							<li><p>Click on installer based on your OS type</p></li>
+						</ol>
+						<h2> Setup Cassandra[NOSQL] . Execute below script </h2>
+						<ol>
+							<li><p>get the DB script from https://academy.datastax.com/planet-cassandra/cassandra and execute in your local DB </p></li>
+						</ol>
+						<h2> For Running UX layer </h2>
+						<ol>
+							<li><p>Clone the repo using <code>git clone https://github.com/bibinkt/ProductService.git</code></p></li>
+							<li><p>Run <code>npm install</code> to install the dependencies.</p></li>
+							<li><p>Run <code>node server.js</code> to start the local web server</p></li>
+						</ol>
+						<h2> For Running API gateway </h2>
+						<ol>
+							<li><p>Clone the repo using <code>git clone https://github.com/bibinkt/nodeAPIGateway.git</code></p></li>
+							<li><p>Run <code>npm install</code> to install the dependencies.</p></li>
+							<li><p>Run <code>node gateway\index.js</code> to start the local web server</p></li>
+						</ol>
+						<h2> For Running Microservices </h2>
+						<ol>
+							<li><p>Clone the repo using <code>git clone https://github.com/bibinkt/ProductService.git</code></p></li>
+							<li><p>Run <code>npm install</code> to install the dependencies.</p></li>
+							<li><p>Run <code>node server.js</code> to start the local web server</p></li>
+						</ol>
+					</section>
+					<section className="text-section">
+						<h2>If you are biginner in React & Redux then strongly recomand you to go thrugh below tutorial </h2>
+						<ul>
+							<li>
+								<p> NodeJS -> https://www.youtube.com/watch?v=-u-j7uqU7sI</p>
+							</li>
+							<li>
+								<p> ReactJS -> https://www.youtube.com/watch?v=-AbaV3nrw6E</p>
+							</li>
+							<li>
+								<p> Redux -> https://www.youtube.com/watch?v=DiLVAXlVYR0</p>
+							</li>
+							<li>
+								<p> ES6 [ECMAScript] -> https://www.youtube.com/watch?v=ZJZfIw3P8No</p>
+							</li>
+						</ul>
+					</section>
+				</div>
+			</article>
+		);
+  }
+}
+
+// Which props do we want to inject, given the global state?
+function select(state) {
+  return {
+    data: state
+  };
+}
+
+// Wrap the component to inject dispatch and state into it
+export default connect(select)(HomePage);
---
+++ js/utils/auth.js
@@ -0,0 +1,173 @@
+
+import 'whatwg-fetch';
+import request from './fakeRequest';
+
+/**
+ * Authentication lib
+ * @type {Object}
+ */
+var auth = {
+  /**
+   * Logs a user in
+   * @param  {string}   username The username of the user
+   * @param  {string}   password The password of the user
+   * @param  {Function} callback Called after a user was logged in on the remote server
+   */
+  login(username, password, callback) {
+    // If there is a token in the localStorage, the user already is
+    // authenticated
+    if (this.loggedIn()) {
+      callback(true);
+      return;
+    }
+    //----------------------------------------------------------------
+    fetch('http://localhost:8000/login/', {
+      method: 'post',
+      mode: "cors",
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'text/plain'
+      },
+      body: JSON.stringify({id: username, pwd: password})
+        }).then(this.checkHttpStatus)
+        .then(this.parseJSON)
+        .then(response => {
+          console.log('success.............'+response.success);
+          if(response.success) {
+            localStorage.token = response.token;
+            callback(true);
+          } else {
+            console.log('Response error111 ===> credential-wrong');
+            var eror1 = {
+              type: "password-wrong"
+            }
+            callback(false,eror1);
+          }
+
+        })
+        .catch(error => {
+          console.log('Response error111 ===> password-wrong');
+          var eror = {
+            type: "password-wrong"
+          }
+          callback(false,eror);
+        })
+
+    //--------------------------------------------------------------------------
+  },
+  register(username, password, callback) {
+    // If there is a token in the localStorage, the user already is
+    // authenticated
+    if (this.loggedIn()) {
+      callback(true);
+      return;
+    }
+    //----------------------------------------------------------------
+    fetch('http://localhost:8000/register/', {
+      method: 'post',
+      mode: "cors",
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'text/plain'
+      },
+      body: JSON.stringify({id: username, pwd: password})
+    }).then(this.checkHttpStatus)
+        .then(this.parseJSON)
+        .then(response => {
+          console.log('success.............'+response.success);
+          if(response.success) {
+            localStorage.token = response.token;
+            callback(true);
+          } else {
+            console.log('Response error111 ===> credential-wrong');
+            var eror1 = {
+              type: "username-exists"
+            }
+            callback(false,eror1);
+          }
+
+        })
+        .catch(error => {
+          console.log('Response error111 ===> username-exists');
+          var eror = {
+            type: "username-exists"
+          }
+          callback(false,eror);
+        })
+
+    //--------------------------------------------------------------------------
+  },
+  /**
+   * Logs the current user out
+   */
+  logout(callback) {
+    request.post('/logout', {}, () => {
+      callback(true);
+    });
+  },
+
+  checkHttpStatus(response) {
+    console.log(''+response.status);
+
+    if (response.status >= 200 && response.status < 300) {
+      return response;
+    } else {
+      var error = new Error(response.statusText)
+      error.response = response;
+      throw error;
+    }
+  },
+
+  parseJSON(response) {
+    return response.json();
+  },
+  /**
+   * Checks if anybody is logged in
+   * @return {boolean} True if there is a logged in user, false if there isn't
+   */
+  loggedIn() {
+    return !!localStorage.token;
+  },
+  loginFake(username, password, callback) {
+    // If there is a token in the localStorage, the user already is
+    // authenticated
+    if (this.loggedIn()) {
+      callback(true);
+      return;
+    }
+    // Post a fake request (see below)
+    request.post('/login', { username, password }, (response) => {
+      // If the user was authenticated successfully, save a random token to the
+      // localStorage
+      if (response.authenticated) {
+        localStorage.token = response.token;
+        callback(true);
+      } else {
+        // If there was a problem authenticating the user, show an error on the
+        // form
+        callback(false, response.error);
+      }
+    });
+  },
+  /**
+   * Registers a user in the system
+   * @param  {string}   username The username of the user
+   * @param  {string}   password The password of the user
+   * @param  {Function} callback Called after a user was registered on the remote server
+   */
+  registerFake(username, password, callback) {
+    // Post a fake request
+    request.post('/register', { username, password }, (response) => {
+      // If the user was successfully registered, log them in
+      if (response.registered === true) {
+        this.login(username, password, callback);
+      } else {
+        // If there was a problem registering, show the error
+        callback(false, response.error);
+      }
+    });
+  },
+  onChange() {}
+}
+
+module.exports = auth;
